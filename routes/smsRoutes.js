0559889a11e9475732a8fde2b3bf0b0758c70a4d
YahooSoja1 <yahoosoja1@gmail.com>
2025-06-20 16:40:03 +0100

    Update smsRoutes.js

--- routes/smsRoutes.js
+++ routes/smsRoutes.js
@@ -1,1 +1,17 @@
+// routes/smsRoutes.js
+const express = require('express');
+const router = express.Router();
+const smsController = require('../controllers/smsController');
 
+// Test route
+router.get('/test', (req, res) => {
+  res.send('✅ SMS Platform API is working');
+});
+
+// Buy number from 5sim or SMS-Activate
+router.post('/buy-number', smsController.buyNumber);
+
+// Get SMS status or code
+router.get('/status/:platform/:id', smsController.getSmsStatus);
+
+module.exports = router;
